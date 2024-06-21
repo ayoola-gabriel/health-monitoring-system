11545615d23530c21c8cbdb5fdf12a35aaec4a6c
Gabriel Ayoola Emmanuel <ayoolagabriel2712@gmail.com>
2024-06-21 09:08:52 +0100

    Security fix

--- sources/firebase.h
+++ sources/firebase.h
@@ -10,11 +10,11 @@ FirebaseConfig config;
 bool taskCompleted = false;
 
 // #include "NotoSansBold15.h"
-#define API_KEY "AIzaSyCnKR1F1quWY5pSKHapDZZmC9Nl_v4okCU"  // Firebase: Define the API Key
-#define USER_EMAIL "ayoolagabriel2712@gmail.com"           // Firebase: Define the user Email
-#define USER_PASSWORD "Newness01#"                         // Firebase: Define password
-#define STORAGE_BUCKET_ID "vtrackfirmware.appspot.com"     // Firebase: Define the Firebase storage bucket ID e.g bucket-name.appspot.com
-#define FIRMWARE_PATH "test/vitalsfirmware.bin"            // Firebase: Define the firmware path on Firebase
+#define API_KEY "YOUR-FIREBASE-API-KEY"  // Firebase: Define the API Key
+#define USER_EMAIL "YOUR-EMAIL"           // Firebase: Define the user Email
+#define USER_PASSWORD "YOUR-LOGIN-PASSWORD"                         // Firebase: Define password
+#define STORAGE_BUCKET_ID "YOUR-STORAGE-BUCKET-ID"     // Firebase: Define the Firebase storage bucket ID e.g bucket-name.appspot.com
+#define FIRMWARE_PATH "YOUR-FIRMAWARE-PATH"            // Firebase: Define the firmware path on Firebase
 
 String swversion = __FILE__;
 
